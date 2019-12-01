471cc8ce89126f540574e8bebdce78d1b121d0c0
Purnima Aggarwal <pgwebdev@gmail.com>
2019-12-01 16:35:57 +0000

    added

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 // Webpack uses this to work with directories
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 // This is main configuration object.
 // Here you write different options and tell Webpack what to do
@@ -66,12 +67,31 @@ module.rules = {
                 ]
             },*/
 
-            {
-                test:/\.(png|jpg|woff|svg|eot|ttf|woff2)$/,
-                loader:'url-loader?limit=8192&name=images/[name].[ext]'
-            }
+                {
+                    // Now we apply rule for images
+                    test: /\.(png|jpeg|gif|svg)$/,
+                    use: [
+                        {
+                            // Using file-loader for these files
+                            loader: "file-loader",
+
+                            // In options we can set different things like format
+                            // and directory to save
+                            options: {
+                                outputPath: 'images'
+                            }
+                        }
+                    ]
+                }
 
         ]
 
-    }
+    },
+    plugins: [
+
+        new MiniCssExtractPlugin({
+            filename: "bundle.css"
+        })
+
+    ]
 }
